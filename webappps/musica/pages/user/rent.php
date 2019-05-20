1dacfee08209f099b0c1a835a3247517f1767bd1
Santosh Kumar Subedi <subedisantos9@gmail.com>
2019-05-21 05:17:50 +0545

    rent table fixed

--- webappps/musica/pages/user/rent.php
+++ webappps/musica/pages/user/rent.php
@@ -1,11 +1,4 @@
 <?php
 $title = 'MULTI MATTE - Films &amp; Recordings';
-
-$productlist = new Database('product_0117');
-$light = $productlist->find('p_type', '1');
-$grips = $productlist->find('p_type', '2');
-$cameras = $productlist->find('p_type', '3');
-$screens = $productlist->find('p_type', '4');
-$lens = $productlist->find('p_type', '5');
-$output = tampletLoader('../templates/user/rentview_templates.html.php',['lights'=>$light,'grips'=>$grips,'cameras'=>$cameras,'screens'=>$screens,'lens'=>$lens]);
+$output = tampletLoader('../templates/user/rentview_templates.html.php',[]);
  ?>
--- webappps/musica/templates/user/rentview_templates.html.php
+++ webappps/musica/templates/user/rentview_templates.html.php
@@ -1,6 +1,7 @@
 <?php
   $categories = new Database('category');
   $category = $categories->findAll();
+  $productlist = new Database('product_0117');
 ?>
 <style>
 table {
@@ -24,6 +25,7 @@ table th {
   background-color: black;
   color: white;
 }
+
 </style>
 <div class="breadcumb-area bg-img bg-overlay2" style="background-image: url(); height: 50px;">
 <div class="bradcumbContent">
@@ -46,10 +48,9 @@ table th {
      <tr>
          <th><?php echo $cat['category_name']; ?></th>
      </tr>
-     </table>
-<?php endforeach; ?>
-         <table style="float: left;">
-           <?php $i=0; foreach($lights as $light){?>
+     <?php $products=$productlist->find('p_type',$cat['category_id']);?>
+
+           <?php $i=0; foreach($products as $light){?>
                 <tr>
                   <td>
                     <?php echo $light['pname']; ?>
@@ -58,50 +59,7 @@ table th {
                 </tr>
            <?php }?>
          </table>
-
-         <table style="float: left;">
-           <?php $i=0; foreach($grips as $grip){?>
-                <tr>
-                  <td>
-                    <?php echo $grip['pname']; ?>
-                    <input type="checkbox" name="grip[<?php echo $i++; ?>]" value="<?php echo $grip['pid']; ?>"
-                  </td>
-                </tr>
-           <?php }?>
-         </table>
-
-         <table style="float: left;">
-           <?php $i=0; foreach($cameras as $camera){?>
-                <tr>
-                  <td>
-                    <?php echo $camera['pname']; ?>
-                    <input type="checkbox" name="camera[<?php echo $i++; ?>]" value="<?php echo $camera['pid']; ?>"
-                  </td>
-                </tr>
-           <?php }?>
-         </table>
-
-         <table style="float: left;">
-           <?php $i=0; foreach($screens as $screen){?>
-                <tr>
-                  <td>
-                    <?php echo $screen['pname']; ?>
-                    <input type="checkbox" name="screen[<?php echo $i++; ?>]" value="<?php echo $screen['pid']; ?>"
-                  </td>
-                </tr>
-           <?php }?>
-         </table>
-
-         <table style="float: left;">
-           <?php $i=0; foreach($lens as $len){?>
-                <tr>
-                  <td>
-                    <?php echo $len['pname']; ?>
-                    <input type="checkbox" name="lens[<?php echo $i++; ?>]" value="<?php echo $len['pid']; ?>"
-                  </td>
-                </tr>
-           <?php }?>
-         </table>
+<?php endforeach; ?>
 
 
 
