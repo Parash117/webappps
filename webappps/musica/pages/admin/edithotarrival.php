18e97307a50f229e2556b207bf4526ebeb963222
Parash Gurung <unforgivingboy@gmail.com>
2019-05-28 01:42:55 +0545

    hot arrivals done

---
+++ webappps/musica/pages/admin/edithotarrival.php
@@ -0,0 +1,9 @@
+<?php
+$title="Admin Home";
+$hot=new Database('hotarrival');
+if(isset($_POST['delete'])){
+  $hot->delete('haid',$_POST['id']);
+}
+$hots=$hot->findAll();
+$output=tampletLoader('../templates/admin/hotarrival_template.html.php',['hot'=>$hots]);
+ ?>
---
+++ webappps/musica/templates/admin/hotarrival_template.html.php
@@ -0,0 +1,43 @@
+<a href="addCover">Add Products</a>
+<style>
+<style>
+table {
+  width:100%;
+}
+table, th, td {
+  border: 1px solid black;
+  border-collapse: collapse;
+}
+th, td {
+  padding: 15px;
+  text-align: left;
+}
+table tr:nth-child(even) {
+  background-color: #eee;
+}
+table tr:nth-child(odd) {
+ background-color: #fff;
+}
+table th {
+  background-color: black;
+  color: white;
+}
+</style>
+</style>
+<?php
+$table = new Table();
+$table->setHeading(['Date','title','<style="width: 10%">Image','Option']);
+foreach ($hot as $row) {
+  $row['titles']=$row['title'];
+unset($row['title']);
+  $row['images']='<img src="../uploads/hotarrivals/'.$row['image'].'" style="height:200px;width:300px;"></img>';
+  unset($row['image']);
+  $row['delete']='<form method="post" action="">
+  <input type="hidden" name="id" value=' . $row['haid'] . ' />
+  <input type="submit" name="delete" value="Delete" />
+  </form>';
+  unset($row['haid']);
+  $table->addRow($row);
+}
+echo $table->getHTML();
+?>
